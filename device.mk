8860937009969e31469eaeb7e9b7e8230d636126
Novicio+MillenniumTeam <157868622+Novicio-2309@users.noreply.github.com>
2026-05-28 17:04:35 +0800

    Comment out compression_with_xor.mk inheritance
    Comment out the line inheriting compression_with_xor.mk for virtual A/B.

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@
 DEVICE_PATH := device/xiaomi/amethyst
 
 # Configure Virtual A/B
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
+# $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression_with_xor.mk)
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
